3fdd374fbae1a8ce052c908ab4648d2051a57adf
vadim.antoshenko <vadim.antoshenko@sectigo.com>
2019-07-23 14:34:15 +0300

    Added: GalToLitTable.java

---
+++ src/main/java/java/a/beginners/guide/chapter01/GalToLitTable.java
@@ -0,0 +1,20 @@
+package main.java.java.a.beginners.guide.chapter01;
+
+public class GalToLitTable {
+    public static void main(String[] args) {
+        double gallons, liters;
+        int counter;
+
+        counter = 0;
+        for (gallons = 1; gallons <= 100; gallons++) {
+            liters = gallons * 3.7854;
+            System.out.println(gallons + " галлонам соответствует " + liters + " литра.");
+
+            counter++;
+            if (counter == 10) {
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+}
