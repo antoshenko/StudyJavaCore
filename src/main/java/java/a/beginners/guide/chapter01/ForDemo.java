7d60fbbe96d344a38a1afbc29b197127ee21f398
vadim.antoshenko <vadim.antoshenko@sectigo.com>
2019-07-23 14:15:40 +0300

    Added: ForDemo.java

---
+++ src/main/java/java/a/beginners/guide/chapter01/ForDemo.java
@@ -0,0 +1,12 @@
+package main.java.java.a.beginners.guide.chapter01;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        int count;
+
+        for (count = 0; count < 5; count = count + 1)
+            System.out.println("Значение счетчика: " + count);
+
+        System.out.println("Готово!");
+    }
+}
