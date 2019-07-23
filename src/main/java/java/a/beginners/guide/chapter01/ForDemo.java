f68b3fbabaa4140b8bf1e871ea3937ea3d2c6673
vadim.antoshenko <vadim.antoshenko@sectigo.com>
2019-07-23 14:19:08 +0300

    Changed: ForDemo.java

--- src/main/java/java/a/beginners/guide/chapter01/ForDemo.java
+++ src/main/java/java/a/beginners/guide/chapter01/ForDemo.java
@@ -4,7 +4,7 @@ public class ForDemo {
     public static void main(String[] args) {
         int count;
 
-        for (count = 0; count < 5; count = count + 1)
+        for (count = 0; count < 5; count++)
             System.out.println("Значение счетчика: " + count);
 
         System.out.println("Готово!");
