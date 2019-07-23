3fde120c285aaa27ab48bbcb8c735bb857564505
vadim.antoshenko <vadim.antoshenko@sectigo.com>
2019-07-23 14:03:33 +0300

    Added: GalToLit.java

---
+++ src/main/java/java/a/beginners/guide/chapter01/GalToLit.java
@@ -0,0 +1,13 @@
+package main.java.java.a.beginners.guide.chapter01;
+
+public class GalToLit {
+    public static void main(String[] args) {
+        double gallons;
+        double liters;
+
+        gallons = 10;
+        liters = gallons * 3.7854;
+
+        System.out.println(gallons + " галлонам соответствует " + liters + " литра");
+    }
+}
