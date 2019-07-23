a268f2f5ba561481690db0237335cdd628a00e91
vadim.antoshenko <vadim.antoshenko@sectigo.com>
2019-07-23 13:12:51 +0300

    Added: Example.java

---
+++ src/main/java/java/a/beginners/guide/chapter01/Example.java
@@ -0,0 +1,7 @@
+package main.java.java.a.beginners.guide.chapter01;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Java правит Интернетом!");
+    }
+}
