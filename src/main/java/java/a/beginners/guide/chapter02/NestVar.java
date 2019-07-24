a4609c280b6c81d3b89dc2f6ec89977f99888c70
vadim.antoshenko <vadim.antoshenko@sectigo.com>
2019-07-24 23:21:21 +0300

    Added: RellogOps.java Changed: NestVar.java

--- src/main/java/java/a/beginners/guide/chapter02/NestVar.java
+++ src/main/java/java/a/beginners/guide/chapter02/NestVar.java
@@ -14,7 +14,7 @@ public class NestVar {
         for (count = 0; count < 10; count++) {
             System.out.println("Значение count: "+ count);
 
-            int count; // Недопустимо!!!
+            // int count; // Недопустимо!!!
 
             for (count = 0; count < 2; count++)
                 System.out.println("В этой программе есть ошибка!");
