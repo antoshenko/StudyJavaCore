1bf9012d95ba4353fa45f5a92c37f90aefea5de6
Dev-Win64-PC1 <vadym.antoshenko@gmail.com>
2019-07-27 13:50:31 +0300

    Changed: Comma.java

--- src/main/java/java/a/beginners/guide/chapter03/Comma.java
+++ src/main/java/java/a/beginners/guide/chapter03/Comma.java
@@ -3,6 +3,6 @@ package main.java.java.a.beginners.guide.chapter03;
 public class Comma {
     public static void main(String[] args) {
         for (int i = 0, j = 10; i < j; i++, j--)
-            System.out.println("i и j: " + i + "" + j);
+            System.out.println("i и j: " + i + " " + j);
     }
 }
