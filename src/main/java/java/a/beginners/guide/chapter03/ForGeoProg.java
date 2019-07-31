083db8355ead140bb982a23c4eeaaa17c65cf4ea
Dev-Win64-PC1 <vadym.antoshenko@gmail.com>
2019-07-31 23:27:37 +0300

    Added: ForGeoProg.java

---
+++ src/main/java/java/a/beginners/guide/chapter03/ForGeoProg.java
@@ -0,0 +1,9 @@
+package main.java.java.a.beginners.guide.chapter03;
+
+public class ForGeoProg {
+    public static void main(String[] args) {
+        for (int i = 1; i < 1025; i *= 2) {
+            System.out.println(i + "  ");
+        }
+    }
+}
