4f21b989827d7898f04bbfb8c9caf4488cface1e
Dev-Win64-PC1 <vadym.antoshenko@gmail.com>
2019-08-01 23:57:13 +0300

    Added: ChkNum.java, ParmDemo.java

--- readme.md
+++ readme.md
@@ -65,3 +65,4 @@ p.143 - Vehicle.java, VehicleDemo.java
 p.144 - TwoVehicle.java
 p.148 - AddMeth.java
 p.152 - RetMeth.java
+p.154 - ChkNum.java, ParmDemo.java
---
+++ src/main/java/java/a/beginners/guide/chapter04/ChkNum.java
@@ -0,0 +1,10 @@
+package main.java.java.a.beginners.guide.chapter04;
+
+public class ChkNum {
+    // Возврат логического значения true,
+    // если x содержит четное число
+    boolean isEven(int x) {
+        if ((x % 2) == 0) return true;
+        else return false;
+    }
+}
