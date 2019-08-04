5f08dd220ae92e1f62556d967ad768a7c8497514
Dev-Win64-PC1 <vadym.antoshenko@gmail.com>
2019-08-04 19:26:03 +0300

    Updated: ForEach.java

--- src/main/java/java/a/beginners/guide/chapter05/ForEach.java
+++ src/main/java/java/a/beginners/guide/chapter05/ForEach.java
@@ -3,12 +3,23 @@ package main.java.java.a.beginners.guide.chapter05;
 class ForEach {
     public static void main(String[] args) {
         int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int sum = 0;
+        int sum;
 
+        sum = 0;
         for (int x : nums) {
             System.out.println("Значение: " + x);
             sum += x;
         }
         System.out.println("Сумма: " + sum);
+
+        System.out.println();
+
+        sum = 0;
+        for (int x : nums) {
+            System.out.println("Значение: " + x);
+            sum += x;
+            if (x==5) break;
+        }
+        System.out.println("Сумма 5 элементов: " + sum);
     }
 }
